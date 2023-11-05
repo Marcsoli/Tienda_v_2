09c443b5e7e924d5e13d0b5a31095c376caff735
Marcos Solis <Marcos Solis@192.168.0.29>
2023-11-04 18:22:10 -0600

    Semana 08 (semana 7 habia examen)

---
+++ Tienda_v-master/src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,32 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data //Pone los set y get...
+@Entity
+@Table(name = "producto")
+public class Producto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id //Llave de la clase
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+    //private Long idCategoria;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private String rutaImagen;
+    private Boolean activo;
+    
+    
+    @ManyToOne
+    @JoinColumn(name="id_categoria")
+    Categoria categoria;
+
+}
+
---
+++ Tienda_v-master/src/main/java/com/tienda/service/FirebaseStorageService.java
@@ -0,0 +1,20 @@
+package com.tienda.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FirebaseStorageService {
+
+    public String cargaImagen(MultipartFile archivoLocalCliente, String carpeta, Long id);
+
+    //El BuketName es el <id_del_proyecto> + ".appspot.com"
+    final String BucketName = "techshop-v1-b90b8.appspot.com";
+
+    //Esta es la ruta básica de este proyecto Techshop
+    final String rutaSuperiorStorage = "techshop";
+
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile = "firebase";
+    
+    //El nombre del archivo Json
+    final String archivoJsonFile = "techshop-v1-b90b8-firebase-adminsdk-kse1r-99ed4cc699.json";
+}
